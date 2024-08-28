fd5c32d76ab34850ddd164b656c72ee2ea3f4f00
Shaik Allabakash <139256843+allabakash486@users.noreply.github.com>
2024-08-28 23:36:44 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,7 +1,6 @@
 string = input('Enter the string: ')
 def subprinting(string):
     for val in range(len(string)):
-        word = ''
         for i in range(val+1,len(string)+1):
             print(string[val:i])
 subprinting(string)
